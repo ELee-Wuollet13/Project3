81348f711fe254fbd177ef791cfd5af8aa8aaf6d
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-11-01 10:33:57 -0700

    reload prior build for refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,40 @@
-// UI Logic
+// Business logic
+
+function glitch(array, num) {
+  var result = [];
+  var array = [];
+  for (i = 0; i <= num; i++) {
+    array.push(i.toString());
+  }
+  for (i = 1; i <= num; i++) {
+    if (array[i].includes("3")) {
+      result.push("I'm sorry dave, I'm affraid I can't do that");
+    } else if (array[i].includes("2")) {
+      result.push("Boop");
+    } else if (array[i].includes("1")) {
+      result.push("Beep");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+// User Interface logic
+
+
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    var input = parseInt($('#input').val());
-    var result = counter(input)
-    alert(result)
-
-
+  $("#input form").submit(function(event) {
+    $(".results").show();
     event.preventDefault();
+    var num = parseInt($('input#intake').val());
+    // var array = [];
+    // for (i = 0; i <= num; i++) {
+    //   array.push(i.toString());
+    // }
+    var results = glitch(num);
+    results.forEach(function(element) {
+  $('#solution').append("<li>" + element + "</li>");
   })
 })
-
-
-// Business logic
+})
