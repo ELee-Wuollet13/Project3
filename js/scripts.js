a70f83fe0aaef9ab553a46a2693ca9ce5f78dd05
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-11-01 10:02:23 -0700

    upkeep

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
+// UI Logic
 $(document).ready(function() {
-  $(form).submit(function(event) {
+  $("form").submit(function(event) {
     var input = parseInt($('#input').val());
-
-
+    var result = counter(input)
+    alert(result)
 
 
     event.preventDefault();
   })
 })
+
+
+// Business logic
